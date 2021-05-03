b696960bd1e72295ddc3edad98b9dcd0d8ab4737
zoutianwei <70244232+zoutianwei@users.noreply.github.com>
2021-05-03 03:47:38 +0000

    Deploying to gh-pages from @ zoutianwei/cloud-music@f76dbfda6bf30e60dbfdb509e2ba811e3fa7f254 🚀

---
+++ precache-manifest.406f67fe15ea169d6d3349f9bb88d919.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db0c2975e41cf47edf",
+    "url": "/css/app.0467bb43.css"
+  },
+  {
+    "revision": "a09ba8c448ca935bd32a",
+    "url": "/css/chunk-vendors.84f17f95.css"
+  },
+  {
+    "revision": "33448d058ff1101c83b410b4b8c29f18",
+    "url": "/img/hot_music_bg_2x.33448d05.png"
+  },
+  {
+    "revision": "0c646b0cf2410dac2bc51ee5cffde86b",
+    "url": "/img/logo-big.0c646b0c.png"
+  },
+  {
+    "revision": "3d413b32ec6642cd5e98d8fe015372a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78db0c2975e41cf47edf",
+    "url": "/js/app.27b46f5a.js"
+  },
+  {
+    "revision": "a09ba8c448ca935bd32a",
+    "url": "/js/chunk-vendors.6f63c5bb.js"
+  },
+  {
+    "revision": "c5e6da218bc416586c759cf6e1cc71b5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
